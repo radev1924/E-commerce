27abe904ff6cf84644c9b3ff79c37a44904e6817
radev1924 <radev1924@abv.bg>
2023-03-09 10:18:54 +0200

    base page

--- ecommerce/pages/index.js
+++ ecommerce/pages/index.js
@@ -3,13 +3,18 @@ import React from 'react'
 const Home = () => {
   return (
     <>
-        HeroBanner
+        HeroBanner 
 
         <div>
           <h2>Best Selling Products</h2>
           <p>lorem</p>
         </div>
-        
+
+        <div>
+          {['Product 1','Product 2'].map((product) => product)} 
+        </div>
+
+        Footer
     </>
   )
 }
