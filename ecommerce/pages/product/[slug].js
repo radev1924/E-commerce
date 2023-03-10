cab20b36eccda8a59f47d629568df281084d0968
radev1924 <radev1924@abv.bg>
2023-03-10 12:46:38 +0200

    i lied

---
+++ ecommerce/pages/product/[slug].js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const ProductDetails = () => {
+  return (
+<div>
+    <div 
+    className="product-detail-container">
+     <div>
+       <div className= "image-container">
+         <img src=" " />
+      </div>
+     </div>
+    </div>
+</div>
+
+  )
+}
+
+export default ProductDetails
