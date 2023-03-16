af03bccbc13b4b8533217f5cc7412d59fb225ce2
radev1924 <radev1924@abv.bg>
2023-03-16 15:20:42 +0200

    resolution

--- ecommerce/pages/api/stripe.js
+++ ecommerce/pages/api/stripe.js
@@ -17,7 +17,7 @@ export default async function handler(req, res) {
             ],
             line_items: req.body.map((item) => {
               const img = item.image[0].asset._ref;
-              const newImage = img.replace('image-', 'https://cdn.sanity.io/images/ny7xdyx1/production/').replace('-webp','.webp')
+              const newImage = img.replace('image-', 'https://cdn.sanity.io/images/ny7xdyx1/production/').replace('-png','.png')
 
               return {
                 price_data: {
@@ -36,8 +36,8 @@ export default async function handler(req, res) {
               }
               
             }),
-            success_url: `${req.headers.origin}/?success=true`,
-            cancel_url: `${req.headers.origin}/?canceled=true`,
+            success_url: `${req.headers.origin}/success`,
+            cancel_url: `${req.headers.origin}/canceled`,
           }
       // Create Checkout Sessions from body params.
       const session = await stripe.checkout.sessions.create(params);
