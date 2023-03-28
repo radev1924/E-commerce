3cc303acf2bde5e7bea1b4dc44f53877ba399ad0
radev1924 <radev1924@abv.bg>
2023-03-28 14:27:21 +0300

    rand

--- ecommerce/pages/success.js
+++ ecommerce/pages/success.js
@@ -27,7 +27,7 @@ const Success = () => {
             <p className='email-msg'>Check your email inbox for the receipt</p>
             <p className='description'> if you have any questions, please email
                 <a className='email' href='mailto:order@example.com'>
-                    order@example.com
+                    radev1924@abv.bg
                 </a>
             </p>
             <Link href='/'>
