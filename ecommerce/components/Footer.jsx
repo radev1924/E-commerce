7d49a34c8158821fae5140cf332af2450a5986ca
radev1924 <radev1924@abv.bg>
2023-06-15 21:25:15 +0300

    social links

--- ecommerce/components/Footer.jsx
+++ ecommerce/components/Footer.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
-import { AiFillInstagram, AiOutlineTwitter } from 'react-icons/ai';
+import Link from 'next/link';
+import { AiFillInstagram, AiOutlineFacebook,AiOutlineGithub } from 'react-icons/ai';
 
 const Footer = () => {
   return (
     <div className='footer-container'>
+      <Link className='icons' href='https://www.instagram.com/radev1924/'><AiFillInstagram/></Link>
+      <Link className='icons' href='https://www.facebook.com/daniel.radev.98/'><AiOutlineFacebook/></Link>
+      <Link className='icons' href='https://github.com/radev1924'><AiOutlineGithub/></Link>
       <p>
         © 2023 - All rights reserved.
       </p>
